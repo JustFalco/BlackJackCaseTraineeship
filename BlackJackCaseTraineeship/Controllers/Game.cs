bbf82c97cf03cef665225516d15325aaa0eeefb6
JustFalco <falco@wolkorte.nl>
2022-10-06 19:25:35 +0200

    refactor repush

--- BlackJackCaseTraineeship/Controllers/Game.cs
+++ BlackJackCaseTraineeship/Controllers/Game.cs
@@ -9,20 +9,66 @@ namespace BlackJackCaseTraineeship.Controllers
 		GameController gameController;
 		TurnController turnController;
 		List<Player> playersInGame;
+		private Dealer dealer;
 		List<CardDeck> cardsInGame;
 		bool IsGameActive = true;
 
-		public Game(GameController gameController, TurnController turnController)
+		public Game()
 		{
-			this.turnController = turnController;
-			this.gameController = gameController;
+			this.turnController = new TurnController();
+			this.gameController = new GameController();
 		}
 
-		public void InitializeGame()
+		public void InitializeGame(int amoundOfPlayers, int amoundOfDecks)
 		{
+			//TODO InitializeGame Function
 			//initialize all players
+			this.playersInGame = initializePlayers(amoundOfPlayers);
+			//set player names
 			//initialize dealer
+			dealer = new Dealer();
+
 			//Set amound of decks
+			this.cardsInGame = initializeCardDecks(amoundOfDecks);
+		}
+
+
+		//TODO refactor naar één initialize functie
+		private List<Player> initializePlayers(int amoundOfPlayers)
+		{
+			List<Player> playerList = new List<Player>();
+
+			for (int i = 0; i < amoundOfPlayers; i++)
+			{
+				playerList.Add(new Player());
+			}
+
+			return playerList;
+		}
+
+		private List<CardDeck> initializeCardDecks(int amoundOfCardDecks)
+		{
+			List<CardDeck> cards = new List<CardDeck>();
+
+			for (int i = 0; i < amoundOfCardDecks; i++)
+			{
+				cards.Add(new CardDeck());
+			}
+
+			return cards;
+		}
+
+		private List<T> initializeList<T>(int amoundOfListItems)
+		{
+			List<T> itemList = new List<T>();
+
+			for (int i = 0; i < amoundOfListItems; i++)
+			{
+				T obj = new T();
+				itemList.Add(obj);
+			}
+
+			return itemList;
 		}
 
 	}
